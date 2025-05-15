7622cbd60b6f94b81a55dfb1a3796d4652a46cac
Mostapha EL ANSARI <138169519+mostaphaelansari@users.noreply.github.com>
2025-05-15 21:53:25 +0200

    review code

--- ml/logistic_regression/__init__.py
+++ ml/logistic_regression/__init__.py
@@ -1,12 +1,12 @@
 """
-Logestic Regression package implemented from scratch.
-This package provides a simple implementation of linear regression
+Logistic Regression package implemented from scratch.
+This package provides a simple implementation of logistic regression
 with gradient descent optimization.
 """
 
-from .model import LogisticRegressionLogisticRegression
+from .model import LogisticRegression
 
-# Define what should be imported with "from linear_regression import *"
+# Define what should be imported with "from logistic_regression import *"
 __all__ = ['LogisticRegression']
 
 # Package metadata
