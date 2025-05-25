32048e98b9b682f9a6303d35aec3be0e2a5a7751
Mostapha EL ANSARI <138169519+mostaphaelansari@users.noreply.github.com>
2025-05-26 00:06:57 +0200

    finishing logistic regression

--- ml/logistic_regression/model.py
+++ ml/logistic_regression/model.py
@@ -11,6 +11,7 @@ class LogisticRegression:
     """
     Logistic Regression implementation from scratch
     """
+    
     def __init__(self, learning_rate=0.01, num_iterations=1000):
         self.learning_rate = learning_rate
         self.num_iterations = num_iterations
