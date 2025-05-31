c091c4e5e998dc008afdb9c323dd8aad53041cdd
Mostapha EL ANSARI <138169519+mostaphaelansari@users.noreply.github.com>
2025-05-31 18:44:35 +0200

    init.py

---
+++ ml/decision_tree/__init__.py
@@ -0,0 +1,16 @@
+"""
+Decision Tree package implemented from scratch.
+
+This package provides a simple implementation of Decision Tree.
+
+Usage:
+    from decision_tree import DecisionTree
+"""
+
+from .model import DecisionTree
+
+__all__ = ['DecisionTree']
+
+__version__ = '0.1.0'
+__author__ = 'Homelander'
+__license__ = 'MIT'
